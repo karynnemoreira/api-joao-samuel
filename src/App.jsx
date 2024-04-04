27a9f6cabae822ae5576b4c94afeafd1f3938cbe
karynnemoreira <karynnemoreira@yahoo.com.br>
2024-04-04 19:39:49 -0300

    aula do João e Samuel sobre API's

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import * as S from "./style.jsx"
+import {GlobalStyle} from "./Global.jsx"
+import Vnw from "./assets/vnw.png"
+
+
+export default function App() {
+
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const resp = await axios.get('https://school-project-hxyp.onrender.com/colaboradores');
+    setData(resp.data)
+    console.log(resp)
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+     <GlobalStyle />
+    
+<S.Navegacao>
+  <img src={Vnw} alt="" />
+  <ul> 
+  <li>Colaboradores</li>
+  <li>Alunos</li>
+  <li>Cadastrar</li>
+  </ul>
+</S.Navegacao>
+
+    <S.Container>
+         
+      {data.map((item) => (
+          <S.Cadastro>
+          {/* <h3>{item.id}</h3> */}
+          <img src={item.foto} alt="" />
+          <h2>{item.nome} </h2>
+          <h3>{item.email}</h3>
+          <h3>{item.cpf}</h3>
+          <h3>{item.cargo}</h3>
+        </S.Cadastro>
+      ))}
+    </S.Container>
+    </>
+  )
+}
---
+++ src/style.jsx
@@ -0,0 +1,90 @@
+import styled from "styled-components"
+
+export const Navegacao = styled.nav`
+display: flex;
+justify-content: space-around;
+align-items: center;
+height: 25vh;
+background-color: #ffd452;
+
+@media (min-width: 320px) and (max-width: 768px){
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 40vh;
+
+    img{
+        height: 15vh;
+    }
+
+    ul{
+        height: 20vh;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
+    }
+}
+
+img{
+    height: 10vh;
+}
+
+ul{
+    width: 60vw;
+    display: flex;
+    justify-content: space-evenly;
+}
+
+li{
+    list-style: none;
+    font-size: 1.7rem;
+    font-weight: 600;
+}
+`
+
+
+
+export const Container = styled.section`
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+
+@media (min-width: 320px) and (max-width: 768px){
+    height: 100vh;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+`
+
+export const Cadastro = styled.div`
+background-color: #ffd452;
+border: solid #ff2e5f;
+border-radius: 10%;
+width: 25vw;
+display: flex;
+flex-direction: column;
+justify-content: space-evenly;
+align-items: center;
+/* background-image: url("https://yata-apix-cbb6b702-ef1a-4258-908c-6cfa93768a5c.s3-object.locaweb.com.br/13ccb0ed6a4c4b8b8d6e06bab0378823.png");
+background-repeat: no-repeat;
+background-position: center;
+background-size: 20%; */
+
+h2{
+    font-size: 2rem;
+}
+
+h3{
+    font-size: 1.5rem;
+    color: #140d1a;
+}
+
+@media (min-width: 320px) and (max-width: 768px){
+width: 80%;
+height: 70vh;
+margin: 1rem;
+}
+
+
+`
